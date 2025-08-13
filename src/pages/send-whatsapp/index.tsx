4ba66ad7dcc951bd2166cd065a916023175f192f
LANDIN\junio <landin@germinareagro.com.br>
2025-08-13 13:16:44 -0300

    GAIA-31: Create page send-whatsapp.

--- src/pages/send-whatsapp/index.tsx
+++ src/pages/send-whatsapp/index.tsx
@@ -1,9 +1,34 @@
+import Button from "@/components/FormComponents/Button";
+import ButtonCadaster from "@/components/FormComponents/ButtonCadaster";
+import TextArea from "@/components/FormComponents/TextArea";
 import Layout from "@/components/Layout";
 
 export default function SendWhatsapp() {
   return (
     <Layout>
-      <div className="text-white">Enviar Mensagem</div>
+      <div className="w-full flex justify-center">
+        <div className="gap-4 max-w-6xl flex flex-col">
+          <h1 className="text-white text-center text-4xl font-bold">Enviar Mensagem via WhatsApp</h1>
+          <p className="text-white">Neste campo, você pode escrever a mensagem que será enviada aos clientes selecionados. No botão abaixo, selecione os clientes para os quais deseja enviar a mensagem.</p>
+          <div className="w-full flex justify-end">
+            <Button
+              title="Selecionar Cliente"
+            />
+          </div>
+          <TextArea
+            placeholder="Digite o texto..."
+          />
+
+          <div className="w-full flex flex-col gap-2">
+            <h5 className="text-white font-bold text-lg">Clientes Selecionados</h5>
+            <p className="text-white font-light">Junior, Gustavo, Jojo, Amanda, Isa, Fernando, João, Alex</p>
+          </div>
+
+          <ButtonCadaster
+            title="Enviar"
+          />
+        </div>
+      </div>
     </Layout>
   )
 }
